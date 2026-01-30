f2240e734f7f8f2ba99450bd913558d08e920751
pointmicro <charlesf167@gmail.com>
2026-01-30 10:07:39 +0000

    components

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,18 +1,25 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-
-
-selector: 'app-home',
-templateUrl: './home.component.html',
-styleUrl: './home.component.scss'
-
-
-
-
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
-
 export class HomeComponent {
+  title = 'Homepage';
 
+  description =
+    'My name is Gabriel, a passionate and detail-oriented Software Developer ' +
+    'with a strong focus on creating secure, user-friendly, and innovative solutions. ' +
+    'My expertise spans application security, mobile development, and full-stack ' +
+    'development, where I use technical to deliver high-quality, scalable software.';
 
+  githubUrl = 'https://github.com/pointmicro';
+  linkedinUrl = 'https://www.linkedin.com/in/gabriel-a-234913248/';
+  email = 'gabe720a@gmail.com';
+
+  githubIcon = 'assets/imgs/github.png';
+  linkedinIcon = 'assets/imgs/linkedin.png';
+  heroImage = 'assets/imgs/pc1.gif';
 }
